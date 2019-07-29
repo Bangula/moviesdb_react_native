82b07f3ca28f479b4fc3f22b5992ad3ac6616ea1
Bangula <bvukovic082@gmail.com>
2019-07-29 12:16:49 +0200

    test

--- src/components/screens/home/Home.js
+++ src/components/screens/home/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, ScrollView, Text, Button, StyleSheet } from "react-native";
 
 const Home = ({ navigation }) => {
   return (
-    <View>
-      <Text style={{ textAlign: "center", color: "#fff" }}>Home Page</Text>
-    </View>
+    <ScrollView>
+      <Text>Home Page</Text>
+    </ScrollView>
   );
 };
 
