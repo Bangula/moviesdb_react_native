afc4fe8b0d7f287c6a23674657b1a6cc3eeb30da
Dalibor Petric <dalibor.petric@quantox.com>
2019-07-29 12:54:58 +0200

    problem

--- src/components/screens/home/Home.js
+++ src/components/screens/home/Home.js
@@ -19,17 +19,3 @@ const Home = ({ navigation }) => {
 };
 
 export default Home;
-// const styles = StyleSheet.create({
-//   container: {
-//     borderRadius: 4,
-//     borderWidth: 0.5,
-//     borderColor: '#d6d7da',
-//   },
-//   title: {
-//     fontSize: 19,
-//     fontWeight: 'bold',
-//   },
-//   activeTitle: {
-//     color: 'red',
-//   },
-// });
--- src/components/screens/home/components/HomeMovie.js
+++ src/components/screens/home/components/HomeMovie.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   View,
+  Image,
   Text,
   Button,
   StyleSheet,
@@ -8,12 +9,35 @@ import {
   TouchableOpacity
 } from "react-native";
 
-const HomeMovie = ({ navigation }) => {
+import img from "../../../../assets/images/lion.jpg";
+
+const HomeMovie = () => {
   return (
-    <View>
-      <Text>HomeMovie Page</Text>
-    </View>
+    <TouchableOpacity
+      style={{ flex: 1 }}
+      //onPress={this._onPressButton}
+    >
+      <Image
+        style={{ width: "100%", height: "100%" }}
+        // style={styles.button}
+        source={img}
+      />
+    </TouchableOpacity>
   );
 };
 
 export default HomeMovie;
+// const styles = StyleSheet.create({
+//   container: {
+//     borderRadius: 4,
+//     borderWidth: 0.5,
+//     borderColor: '#d6d7da',
+//   },
+//   title: {
+//     fontSize: 19,
+//     fontWeight: 'bold',
+//   },
+//   activeTitle: {
+//     color: 'red',
+//   },
+// });
